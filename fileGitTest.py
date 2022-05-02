18573872679fde2e75b035e62d521cc2f3a8ff90
Tage Maltby <Tagemaltby@gmail.com>
2022-05-02 09:08:54 +0200

    added my first file

---
+++ fileGitTest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 08:25:43 2022
+
+@author: Tage Maltby
+"""
+import numpy as np
+liste = [i for i in range(18,50)]
+print(liste,"lengde:", len(liste))
+streng = "shockPosition.txt"
+print(streng[-4:])
+
+li = np.array([1,2,3,4,5,6])
+print(li[:3])
+b = list(li)
